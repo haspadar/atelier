d688b8e189feb51b13ed99baca76b42d8d967dda
Kanstantsin <haspadar@gmail.com>
2022-10-28 23:28:36 +0300

    Added message telegram checking

--- public/telegram/index.php
+++ public/telegram/index.php
@@ -3,9 +3,11 @@ require_once '../../vendor/autoload.php';
 date_default_timezone_set('Europe/Minsk');
 
 $basicChatData = json_decode(file_get_contents("php://input"),true);
-$chatId = $basicChatData['message']['chat']['id'];
-$message = $basicChatData['message']['text'] ?? '';
-$response = send($message . ': Hello my friend!. debug:' . var_export($basicChatData, true), $chatId);
+if (isset($basicChatData['message'])) {
+    $chatId = $basicChatData['message']['chat']['id'];
+    $message = $basicChatData['message']['text'] ?? '';
+    $response = send($message . ': Hello my friend!. debug:' . var_export($basicChatData, true), $chatId);
+}
 
 function send(string $message, string $chatId) {
     $token = \Atelier\Settings::getByName('telegram_token');
