bb3de206fd637320a8547a7a32e9f7884e23e125
Kanstantsin <haspadar@gmail.com>
2022-11-05 23:03:00 +0300

    Added notification new line

--- src/Notifications.php
+++ src/Notifications.php
@@ -56,7 +56,7 @@ class Notifications
         $url = sprintf('<a href="%s">Перейти на сайт</a>', self::generateUrl($type));
         $groupTitles = self::getGroupTitles($checks);
         $list = implode(', ', array_map(
-            fn($name) => urlencode('– "' . $name . PHP_EOL),
+            fn($name) => '– "' . $name . urlencode(PHP_EOL),
             array_slice($groupTitles, 0, 20)
         ));
 
