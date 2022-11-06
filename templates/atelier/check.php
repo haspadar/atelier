6f878904c14bf83c5cb46cbd3bfe1087d260fd8d
Kanstantsin <haspadar@gmail.com>
2022-11-06 17:55:45 +0300

    Fixed deleted check vire

--- templates/atelier/check.php
+++ templates/atelier/check.php
@@ -11,7 +11,11 @@ use Atelier\Time; ?>
  */
 
 $check = $this->data['check'];
-if ($check->isIgnored()) :
+if (!$check) :?>
+    <div class="alert alert-danger alert-dismissible mt-2 fade show" role="alert">
+        Сообщение удалено
+    </div>
+<?php elseif ($check->isIgnored()) :
 ?>
     <div class="alert alert-danger alert-dismissible mt-2 fade show" role="alert">
         Сообщение скрыто
@@ -20,52 +24,54 @@ if ($check->isIgnored()) :
 endif;
 ?>
 
-<dl>
-    <dt>Заголовок</dt>
-    <dd>
-        <?=$check->getGroupTitle()?>
-    </dd>
-    <dt>Текст</dt>
-    <dd>
-        <?=$check->getText()?>
-    </dd>
-        <?php if ($check->getProjectId()) :?>
-            <dt>Проект</dt>
-            <dd class="text-muted">
-                <a href="/projects/<?=$check->getProjectId()?>" target="_blank" class="text-decoration-none">
-                    <?= $check->getProjectName()?>
-                </a>
+<?php if ($check) :?>
+    <dl>
+        <dt>Заголовок</dt>
+        <dd>
+            <?=$check->getGroupTitle()?>
+        </dd>
+        <dt>Текст</dt>
+        <dd>
+            <?=$check->getText()?>
+        </dd>
+            <?php if ($check->getProjectId()) :?>
+                <dt>Проект</dt>
+                <dd class="text-muted">
+                    <a href="/projects/<?=$check->getProjectId()?>" target="_blank" class="text-decoration-none">
+                        <?= $check->getProjectName()?>
+                    </a>
 
-                <button class="btn btn-danger btn-sm delete-project" data-id="<?=$check->getProjectId()?>">Удалить проект</button>
-            </dd>
-        <?php endif;?>
+                    <button class="btn btn-danger btn-sm delete-project" data-id="<?=$check->getProjectId()?>">Удалить проект</button>
+                </dd>
+            <?php endif;?>
 
-    <dt>Машина</dt>
-    <dd class="text-muted">
-        <a href="/machines/<?=$check->getMachineId()?>" target="_blank" class="text-decoration-none">
-            <?= $check->getMachineHost()?>
-        </a>
-    </dd>
-    <dt>Время проверки</dt>
-    <dd class="text-muted">
-        <?= Time::timeHuman($check->getCreateTime())?>
-    </dd>
-</dl>
+        <dt>Машина</dt>
+        <dd class="text-muted">
+            <a href="/machines/<?=$check->getMachineId()?>" target="_blank" class="text-decoration-none">
+                <?= $check->getMachineHost()?>
+            </a>
+        </dd>
+        <dt>Время проверки</dt>
+        <dd class="text-muted">
+            <?= Time::timeHuman($check->getCreateTime())?>
+        </dd>
+    </dl>
 
-<?php if (!$check->isIgnored()) :?>
-    <button class="btn btn-danger ignore-check" type="button" data-id="<?=$check->getId()?>">
-        Игнорировать сообщение
+    <?php if (!$check->isIgnored()) :?>
+        <button class="btn btn-danger ignore-check" type="button" data-id="<?=$check->getId()?>">
+            Игнорировать сообщение
+        </button>
+    <?php endif;?>
+
+    <?php if ($check->getProjectId()) :?>
+        <button class="btn btn-warning ignore-check-project" type="button" data-id="<?=$check->getId()?>">
+            Не проверять проект <?=$check->getProjectName()?>
+        </button>
+    <?php endif;?>
+
+    <button class="btn btn-warning ignore-check-machine" type="button" data-id="<?=$check->getId()?>">
+        Не проверять машину <?=$check->getMachineHost()?>
     </button>
-<?php endif;?>
-
-<?php if ($check->getProjectId()) :?>
-    <button class="btn btn-warning ignore-check-project" type="button" data-id="<?=$check->getId()?>">
-        Не проверять проект <?=$check->getProjectName()?>
-    </button>
-<?php endif;?>
-
-<button class="btn btn-warning ignore-check-machine" type="button" data-id="<?=$check->getId()?>">
-    Не проверять машину <?=$check->getMachineHost()?>
-</button>
+<?php endif;
 
 
